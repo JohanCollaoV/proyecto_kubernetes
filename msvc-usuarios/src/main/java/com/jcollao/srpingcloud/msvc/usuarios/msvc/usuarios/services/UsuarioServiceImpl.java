c0d7042c33d78d8b98a2842ff09ee91b3d6fd3ef
jcollaokdu <jcollao@kdu.cl>
2026-01-20 12:33:47 -0300

    Fix error clase UsuarioServiceImpl - linea 16

--- msvc-usuarios/src/main/java/com/jcollao/srpingcloud/msvc/usuarios/msvc/usuarios/services/UsuarioServiceImpl.java
+++ msvc-usuarios/src/main/java/com/jcollao/srpingcloud/msvc/usuarios/msvc/usuarios/services/UsuarioServiceImpl.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 public class UsuarioServiceImpl implements UsuarioService {
 
     @Autowired
-    private UsuarioRepository repository
+    private UsuarioRepository repository;
 
     @Override
     @Transactional(readOnly = true)
