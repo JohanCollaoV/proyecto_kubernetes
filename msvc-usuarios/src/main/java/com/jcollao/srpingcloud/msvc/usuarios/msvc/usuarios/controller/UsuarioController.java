efeef1a92655651af009d881dc5e8702364dd60b
jcollaokdu <jcollao@kdu.cl>
2026-01-21 17:23:49 -0300

    Se termina el Controller UsuarioController con sus funciones CRUD

--- msvc-usuarios/src/main/java/com/jcollao/srpingcloud/msvc/usuarios/msvc/usuarios/controller/UsuarioController.java
+++ msvc-usuarios/src/main/java/com/jcollao/srpingcloud/msvc/usuarios/msvc/usuarios/controller/UsuarioController.java
@@ -3,11 +3,9 @@ package com.jcollao.srpingcloud.msvc.usuarios.msvc.usuarios.controller;
 import com.jcollao.srpingcloud.msvc.usuarios.msvc.usuarios.models.entity.Usuario;
 import com.jcollao.srpingcloud.msvc.usuarios.msvc.usuarios.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -29,6 +27,44 @@ public class UsuarioController {
         Optional<Usuario> usuarioOptional = service.porId(id);
         if (usuarioOptional.isPresent()){
             return ResponseEntity.ok().body(usuarioOptional.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+
+    /* Forma de Crear un post
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Usuario cear(@RequestBody Usuario usuario){
+        return service.guardar(usuario);
+    }
+     */
+
+    //Forma de crear un post con ResponseEntity
+    @PostMapping
+    public ResponseEntity<?> crear(@RequestBody Usuario usuario){
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.guardar(usuario));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> editar(@RequestBody Usuario usuario, @PathVariable Long id){
+        Optional<Usuario> o = service.porId(id);
+        if(o.isPresent()){
+            Usuario usuarioDb = o.get();
+            usuarioDb.setNombre(usuario.getNombre());
+            usuarioDb.setEmail(usuario.getEmail());
+            usuarioDb.setPassword(usuarioDb.getPassword());
+            return  ResponseEntity.status(HttpStatus.CREATED).body(service.guardar(usuarioDb));
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminar(@PathVariable Long id){
+        Optional<Usuario> o = service.porId(id);
+        if(o.isPresent()){
+            service.eliminar(id);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
         }
         return ResponseEntity.notFound().build();
     }
